5723a6001705d817f80e353a59a636554778cbcd
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-12-21 19:11:30 +0100

    getters and setters for adoption contract

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,6 +2,15 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Adoption {
-  constructor() public {
+  address[16] public adopters;
+
+  function getAdopters() public view returns(address[16] memory){
+    return adopters;
+  }
+
+  function adopt(uint petId) public returns(uint){
+    require(petId >= 0 && petId <= 15);
+    adopters[petId] = msg.sender;
+    return petId;
   }
 }
